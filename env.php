e9ac44c960617d7dd4fe29de895f15759fce0d13
Olarte96 <rodriguez.olarte96@gmail.com>
2025-08-08 17:02:56 -0500

    se cambia gitignore

---
+++ env.php
@@ -0,0 +1,18 @@
+; <?php exit; ?>
+
+; URL base del proyecto, algo como https://sitio.com
+BASE_URL = "http://localhost/cotizaciones_seotech"
+
+USUARIO_MYSQL = "root"
+PASS_MYSQL = ""
+NOMBRE_BD_MYSQL = "Cotizaciones2"
+HOST_MYSQL = "localhost"
+
+USUARIO_MYSQL_SESION = "root"
+PASS_MYSQL_SESION = ""
+NOMBRE_BD_MYSQL_SESION = "Cotizaciones2"
+HOST_MYSQL_SESION = "localhost"
+
+ID_SEGUIMIENTO = ""
+AD_CLIENT = ""
+AD_SLOT = ""
